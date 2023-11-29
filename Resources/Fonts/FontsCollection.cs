51f1774c779e0c5e7c3c372262a849fb58ac5bcc
Simon Ögaard <Zhyosgames@gmail.com>
2023-11-29 16:14:51 +0100

    font fixes

---
+++ Resources/Fonts/FontsCollection.cs
@@ -0,0 +1,12 @@
+﻿using Avalonia.Media.Fonts;
+
+namespace IsoniaCore.Resources.Fonts;
+
+public sealed class FontsCollection : EmbeddedFontCollection
+{
+    public FontsCollection() : base(
+        new Uri("fonts:Inter", UriKind.Absolute),
+        new Uri("avares://Avalonia.Fonts.Inter/Assets", UriKind.Absolute))
+    {
+    }
+}
---
+++ Resources/Fonts/FontsExtension.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia;
+
+namespace IsoniaCore.Resources.Fonts;
+
+public static class FontsExtension
+{
+    public static AppBuilder WithFont(this AppBuilder appBuilder)
+    {
+        return appBuilder.ConfigureFonts(fontManager =>
+        {
+            fontManager.AddFontCollection(new FontsCollection());
+        });
+    }
+}
