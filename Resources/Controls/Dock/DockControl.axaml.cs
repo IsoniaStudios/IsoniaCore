652d470fdfe1b9b39e8acf12ab26c23169399d38
Simon <Zhyosgames@gmail.com>
2024-01-16 18:38:01 +0100

    test

---
+++ Resources/Controls/Dock/DockControl.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace IsoniaCore.Resources.Controls.Dock;
+
+public partial class DockControl : UserControl
+{
+    public DockControl()
+    {
+        InitializeComponent();
+    }
+}
